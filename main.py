a588b4d55a1b17d0c706aae849187671dd030e6d
mdaniela62 <mdaniela.gentilin@gmail.com>
2025-06-12 13:50:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
-from urllib.parse import urljoin
 from datetime import datetime
 
-BASE_URL = "https://www.comune.altissimo.vi.it/home/novita.html"
+# URL della pagina novità del Comune di Altissimo
+URL = "https://www.comune.altissimo.vi.it/home/novita.html"
 
+# Recupera la pagina
+response = requests.get(URL)
+response.raise_for_status()
 
-def fetch_articles():
-    response = requests.get(BASE_URL)
-    response.raise_for_status()
+soup = BeautifulSoup(response.content, "lxml")
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+# Generazione feed
+fg = FeedGenerator()
+fg.title("Comune di Altissimo - Novità")
+fg.link(href=URL, rel='alternate')
+fg.description("Ultime novità dal sito ufficiale del Comune di Altissimo")
+fg.language('it')
 
-    items = []
+# Trova le notizie
+news_items = soup.select("div.news li a")
 
-    for link in soup.select(".elenco_novita .media-body a"):
-        title = link.get_text(strip=True)
-        url = urljoin(BASE_URL, link.get("href"))
-        items.append({"title": title, "url": url})
+for item in news_items:
+    title = item.get_text(strip=True)
+    link = item['href']
+    if not link.startswith('http'):
+        link = "https://www.comune.altissimo.vi.it" + link
 
-    return items
+    fe = fg.add_entry()
+    fe.title(title)
+    fe.link(href=link)
+    fe.pubDate(datetime.utcnow())  # Non c'è data nel sito ➔ mettiamo data attuale
 
-
-def generate_feed(items):
-    fg = FeedGenerator()
-    fg.title("Comune di Altissimo - Novità")
-    fg.link(href=BASE_URL, rel='alternate')
-    fg.description("Ultime novità dal sito ufficiale del Comune di Altissimo")
-
-    pubdate = datetime.utcnow()
-
-    for item in items:
-        fe = fg.add_entry()
-        fe.title(item['title'])
-        fe.link(href=item['url'])
-        fe.pubDate(pubdate)
-
-    return fg.rss_str(pretty=True)
-
-
-if __name__ == "__main__":
-    articles = fetch_articles()
-    rss_feed = generate_feed(articles)
-
-    with open("altissimo.xml", "wb") as f:
-        f.write(rss_feed)
+# Salva il feed RSS in un file XML
+fg.rss_file("altissimo.xml")
