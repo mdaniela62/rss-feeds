1b36400af649dbe2b124ffb985ace3a888fce448
mdaniela62 <mdaniela.gentilin@gmail.com>
2025-06-28 14:43:18 +0200

    Update Barbarano.py

--- Barbarano.py
+++ Barbarano.py
@@ -46,7 +46,7 @@ try:
 
         print(f"✅ Aggiunto articolo: {title} → {link}")
 
-    fg.rss_file("feed_barbarano.xml")
+    fg.rss_file("barbarano.xml")
     print("✅ Feed generato correttamente per Comune di Barbarano Mossano")
 
 except Exception as e:
