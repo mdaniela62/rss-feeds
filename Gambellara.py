7a8ff179a741713f335942ca6b076ae5492e7e91
mdaniela62 <mdaniela.gentilin@gmail.com>
2025-06-28 15:47:08 +0200

    Update Gambellara.py

--- Gambellara.py
+++ Gambellara.py
@@ -9,7 +9,7 @@ def genera_feed_gambellara():
 
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
             context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
             page = context.new_page()
             page.goto("https://www.comune.gambellara.vi.it/home/novita", timeout=20000)
