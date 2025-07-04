9ba482f41754393a769c7817310956db2e8eea97
mdaniela62 <mdaniela.gentilin@gmail.com>
2025-07-04 16:30:19 +0200

    Update Castelgomberto_Playwright.py

--- Castelgomberto_Playwright.py
+++ Castelgomberto_Playwright.py
@@ -13,7 +13,7 @@ def genera_feed_castelgomberto():
         base_url = "https://www.comune.castelgomberto.vi.it"
 
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)  
+            browser = p.chromium.launch(headless=False)  
             page = browser.new_page()
             page.goto(url, timeout=60000)
             time.sleep(5)
